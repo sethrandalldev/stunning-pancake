497cf93806d66a95b4cc913c3995bc8b073c5d39
Seth Randall <sethjrandall@gmail.com>
2021-05-07 23:09:38 -0500

    [ADDED] name to user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 const User = mongoose.model('User', {
+  name: String,
   email: String,
   password: String,
   location: String
--- server.js
+++ server.js
@@ -11,25 +11,29 @@ mongoose.Promise = Promise;
 const dbUrl = 'mongodb+srv://user:39h89r3@learning-node.lbsjp.mongodb.net/weather?retryWrites=true&w=majority';
 
 app.use(cors({
-    origin: 'http://locahost:3000'
+    origin: 'http://localhost:3000'
 }));
+
+app.use(express.json());
 
 app.post('/login', async(req, res) => {
   User.findOne({email: req.body.email}, (err, user) => {
     bcrypt.compare(req.body.password, user.password, function(err, result) {
-      if (result) res.send({ id: user._id, email: user.email });
+      if (result) res.send({ id: user._id, email: user.email, name: user.name });
       else res.sendStatus(404);
     });
   });
 });
 
 app.post('/register', async(req, res) => {
+  Object.keys(req).forEach(key => console.log(key));
   User.findOne({email: req.body.email}, function (err, user) {
     if (user && Object.keys(user).length) {
       return res.sendStatus(409);
     } else {
       bcrypt.hash(req.body.password, 10, function(err, hash) {
         const user = new User({
+          name: req.body.name,
           email: req.body.email,
           password: hash
         });
